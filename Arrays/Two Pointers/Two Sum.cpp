8dca39f87fbec5dfdaabbf75723b125b2508f80a
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2026-08-13 23:13:16 +0530

    Add/Update solution: Two Sum [Arrays / Two Pointers]

---
+++ Arrays/Two Pointers/Two Sum.cpp
@@ -0,0 +1,39 @@
+/**
+ * Problem: Two Sum
+ * Platform: LeetCode
+ * URL: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ * Difficulty: Medium
+ * Topic: Arrays
+ * Pattern: Two Pointers
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Approach:
+ * N/A
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return {left + 1, right + 1};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return {};
+    }
+};
